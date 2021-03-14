6a9c9de9ca4ce37f62d5279ac46ab1bbd2c8b210
Miller Cesar de Oliveira <miller00315@gmail.com>
2021-03-14 14:47:27 -0300

    Ajustando browser syc

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,25 @@ const gulp = require('gulp');
 const concat = require('gulp-concat');
 const cssmin = require('gulp-cssmin');
 const uncss = require('gulp-uncss');
+var browserSync = require('browser-sync').create();
+
+gulp.task('browserSync', function(){
+    browserSync.init({
+        proxy: 'localhost:5000'
+    });
+
+    gulp.watch('./styles/*.css', gulp.series('css'));
+    gulp.watch('./js/*.js', gulp.series('js'));
+})
 
 gulp.task('js', function(){
     return gulp.src([
         './node_modules/bootstrap/dist/js/bootstrap.min.js',
         './node_modules/jquery/dist/jquery.min.js',
+        './js/site.js',
     ])
-    .pipe(gulp.dest('wwwroot/js/'));
+    .pipe(gulp.dest('wwwroot/js/'))
+    .pipe(browserSync.stream());
 });
 
 gulp.task('css', function(){
@@ -19,9 +31,10 @@ gulp.task('css', function(){
     .pipe(concat('site.min.css'))
     .pipe(cssmin())
     .pipe(uncss({html:['Views/**/*.cshtml']}))
-    .pipe(gulp.dest('wwwroot/css'));
+    .pipe(gulp.dest('wwwroot/css'))
+    .pipe(browserSync.stream());
 });
 
 gulp.task('watch-css', function(){
-    gulp.watch('./styles/**/*.css', ['css']);
+    gulp.watch('./styles/*.css', ['css']);
 });
